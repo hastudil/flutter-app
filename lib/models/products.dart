67f7f31d06f28c8ace47033d55197c164d198a48
hastudil <wolves_86@hotmail.com>
2022-03-21 15:24:17 -0500

    Clean code

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -4,31 +4,8 @@ class ProducstImported {
   final String description;
   final String qty;
 
-  /*ProducstImported.fromMap(Map<String, dynamic> data){
-    id = data['id'];
-    comp = data['comp'];
-    description = data['description'];
-    qty = data['qty'];
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name':comp,
-      'categories': description,
-      'qty': qty
-    };
-  }*/
-
   ProducstImported(this.id, this.comp, this.description, this.qty);
-
-  /*ProducstImported.fromJson(Map<String, dynamic> json) : 
-    id = json['id'],
-    comp = json['comp'],
-    description = json['description'],
-    qty = json['qty'];*/
-
-  //factory ProducstImported.fromJson(dynamic json) {
+  
   factory ProducstImported.fromJson(Map<String, dynamic> json) {
     return ProducstImported(
         "${json['id']}",
@@ -37,14 +14,6 @@ class ProducstImported {
         "${json['qty']}"
     );
   }
-
-  // Method to make GET parameters.
-  /*Map toJson() => {
-    'name': id,
-    'email': comp,
-    'mobileNo': description,
-    'feedback': qty
-  };*/
 
   Map<String,dynamic> toMap() => {
     "id": id,
