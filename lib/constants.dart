bdbb22906cdc162e59a5dba39a7295dbccd7d792
hastudil <wolves_86@hotmail.com>
2022-03-21 14:11:02 -0500

    Added new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const singInBtnColor = Colors.amber;
 const singUpBtnColor = Colors.black26;
 const textBtnColor   = Colors.black;
+const appBarBackground = Colors.black45;
+const signOutColor  = Colors.white;
+const loginTextBtn = "LOGIN";
+const signUpTextBtn = "SIGN UP";
