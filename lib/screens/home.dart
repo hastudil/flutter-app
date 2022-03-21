57b662c06eb3c87a3a66fdbeda3cf5a561b824bb
hastudil <wolves_86@hotmail.com>
2022-03-21 13:51:16 -0500

    Clean code and implemented new constants

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,9 +2,7 @@ import 'package:access_challenge/components/rounded_button.dart';
 import 'package:access_challenge/constants.dart';
 import 'package:access_challenge/screens/login/login_screen.dart';
 import 'package:access_challenge/screens/signup/signup_screen.dart';
-//import 'package:access_challenge/services/auth.dart';
 import 'package:flutter/material.dart';
-//import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -20,7 +18,6 @@ class _HomeState extends State<Home> {
       debugShowCheckedModeBanner: false,
       title: 'Access Challenge',
       theme: ThemeData(
-        //primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const Scaffold(
@@ -28,40 +25,17 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-
-
-  /*@override
-  Widget build(BuildContext context) {
-    final authService = Provider.of<AuthService>(context);
-
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('Cerrar Sesión ${authService.user.displayName}'),
-          onPressed: () {
-            authService.signOut();
-          },
-        ),
-      ),
-    );
-  }*/
 }
 
 class AuthTypeSelector extends StatelessWidget{
-  //final authService = Provider.of<AuthService>(context);
   const AuthTypeSelector({Key? key}) : super(key: key);
-  /*void _pushPage(BuildContext context, Widget page){
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }*/
 
   @override
   Widget build(BuildContext context){
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black45,
+        backgroundColor: appBarBackground,
         centerTitle: true,
         title: const Text(
           'Welcome to Access Challenge',
@@ -79,29 +53,29 @@ class AuthTypeSelector extends StatelessWidget{
             ),
           ),
           RoundedButton(
-            text: "LOGIN",
-            textColor: Colors.black,
+            text: loginTextBtn,
+            textColor: textBtnColor,
             press: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) {
-                    return const LoginScreen();//SignInPage();
+                    return const LoginScreen();
                   },
                 ),
               );
             },
           ),
           RoundedButton(
-            text: "SIGN UP",
+            text: signUpTextBtn,
             color: singUpBtnColor,
-            textColor: Colors.black,
+            textColor: textBtnColor,
             press: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) {
-                    return SignUpScreen();//RegisterPage();
+                    return const SignUpScreen();
                   },
                 ),
               );
